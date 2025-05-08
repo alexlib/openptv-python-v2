b968fdb0a5401cbdc8cd6abbc82106dbf3e4274d
OpenPTV Developer <developer@openptv.org>
2025-05-08 19:55:18 +0300

    Fix segmentation.pyx Cython directives

--- openptv/binding/segmentation.pyx
+++ openptv/binding/segmentation.pyx
@@ -1,4 +1,7 @@
-# cython: language_level=3\n# distutils: language = c\n\n# -*- coding: utf-8 -*-
+# cython: language_level=3
+# distutils: language = c
+
+# -*- coding: utf-8 -*-
 """
 Bindings for image segmentation / target recognition routins in liboptv.
 
@@ -24,14 +27,14 @@ ctypedef np.uint8_t DTYPE_t  # Changed from int_t to uint8_t to match DTYPE
 from openptv.binding.parameters cimport TargetParams, ControlParams
 from openptv.binding.tracking_framebuf cimport TargetArray
 
-def target_recognition(np.ndarray[np.uint8_t, ndim=2] img, TargetParams tpar, int cam, 
+def target_recognition(np.ndarray[np.uint8_t, ndim=2] img, TargetParams tpar, int cam,
     ControlParams cparam, subrange_x=None, subrange_y=None):
     """
     Detects targets (contiguous bright blobs) in an image.
-    
+
     Limited to ~20,000 targets per image for now. This limitation comes from
     the structure of underlying C code.
-    
+
     Arguments:
     np.ndarray img - a numpy array holding the 8-bit gray image.
     TargetParams tpar - target recognition parameters s.a. size bounds etc.
@@ -42,7 +45,7 @@ def target_recognition(np.ndarray[np.uint8_t, ndim=2] img, TargetParams tpar, in
         between. Default is to search entire image width.
     subrange_y - optional, tuple of min and max pixel coordinates to search
         between. Default is to search entire image height.
-    
+
     Returns:
     A TargetArray object holding the targets found.
     """
@@ -52,7 +55,7 @@ def target_recognition(np.ndarray[np.uint8_t, ndim=2] img, TargetParams tpar, in
         target *targs = <target *> calloc(1024*20, sizeof(target))
         int num_targs
         int xmin, xmax, ymin, ymax
-    
+
 
 
     # Set the subrange (to default if not given):
@@ -60,26 +63,26 @@ def target_recognition(np.ndarray[np.uint8_t, ndim=2] img, TargetParams tpar, in
         xmin, xmax = 0, cparam._control_par[0].imx
     else:
         xmin, xmax = subrange_x
-    
+
     if subrange_y is None:
         ymin, ymax = 0, cparam._control_par[0].imy
     else:
         ymin, ymax = subrange_y
-    
+
     if img.shape[0] != ymax or img.shape[1] != xmax:
         raise ValueError("dimensions are not correct")
 
     assert img.dtype == DTYPE
 
     # The core liboptv call:
-    num_targs = targ_rec(<unsigned char *>img.data, tpar._targ_par, 
+    num_targs = targ_rec(<unsigned char *>img.data, tpar._targ_par,
         xmin, xmax, ymin, ymax, cparam._control_par, cam, targs)
-    
+
     # Fit the memory size snugly and generate the Python return value.
     ret = <target *>realloc(targs, num_targs * sizeof(target))
     if ret == NULL:
         free(targs)
         raise MemoryError("Failed to reallocate target array.")
-    
+
     t.set(ret, num_targs, 1)
     return t
